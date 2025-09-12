475464c6784e556b734936243050003bee04a53e
ogchy <98388165+ogchy@users.noreply.github.com>
2025-09-12 15:18:05 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './Header/Header';
+import { HomePage } from './HomePage/HomePage';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Favorites } from './Favorites/Favorites';
+import { Footer } from './Footer/Footer';
+import { Phones } from './Phones/Phones';
+import { Tablets } from './Tablets/Tablets';
+import { Accessories } from './Accessories/Accessories';
+import { ItemCard } from './ItemCard/ItemCard';
+import { Cart } from './Cart/Cart';
+import { CartProvider } from './UseCart/UseCart';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <CartProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/phones" element={<Phones />} />
+              <Route path="/tablets" element={<Tablets />} />
+              <Route path="/product/:productId" element={<ItemCard />} />
+              <Route path="/accessories" element={<Accessories />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 };
